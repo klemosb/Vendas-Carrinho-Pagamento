e2d30fc491c92eea248d0686600802fbcc7f9eef
klemosb <email@example.com>
2023-09-24 13:27:40 -0300

    Retornando as rotas de pagamento apagadas

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import CarrinhoController from '../controllers/carrinhoController.js';
 import VendasController from '../controllers/VendasController.js';
+import PagamentoController from '../controllers/pagController.js'
 
 const router = express.Router();
 
@@ -14,6 +15,15 @@ router.get('/vendas/:id', VendasController.listarVendasPorId);
 router.post('/vendas', VendasController.cadastrarVendas);
 router.put('/vendas/:id', VendasController.atualizarVendas);
 router.delete('/vendas/:id', VendasController.excluirVendas);
+
+
+
+router.get('/pagamento', PagamentoController.listarPagamentos)
+router.get('/pagamento/:id', PagamentoController.listarPagamentoPorId)
+router.get('/pagamento/:venda', PagamentoController.listarPagamentoPorVenda)
+router.post('/pagamento', PagamentoController.cadastrarPagamento)
+router.put('/pagamento/:id', PagamentoController.atualizarPagamento)
+router.delete('/pagamento/:id', PagamentoController.excluirPagamento);
 
 
 export default router;
