cd84a485b645afbee585698030c9146aca13d4d5
klemosb <email@example.com>
2023-11-07 19:41:35 -0300

    alterando name

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import CarrinhoController from '../controllers/carrinhoController.js';
-import VendasController from '../controllers/VendasController.js';
+// import VendasController from '../controllers/VendasController.js';
 import PagamentoController from '../controllers/pagController.js'
+import VendasController from '../controllers/VendasController.js'
+
 
 const router = express.Router();
 
